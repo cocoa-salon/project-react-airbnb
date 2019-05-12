008aaa9b1b95bd312e2fa8f742a2fc899bf3fc0b
cocoa <>
2019-05-12 17:58:20 +0900

    27(main) 기능 개선(검색 옵션 탭 스타일)
     - 검색 옵션 탭이 활성화된 상태에서 마우스 호버시 스타일 변경(배경)
    
     - styled component 내 함수 추가

--- src/App/components/Main/SearchPanel/SearchTabs/SearchOptionTabs/SearchOptionTabs.js
+++ src/App/components/Main/SearchPanel/SearchTabs/SearchOptionTabs/SearchOptionTabs.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { SearchOptionTabStyle } from './SearchOptionTabStyle';
+import styled from 'styled-components';
+// import { SearchOptionTabStyle } from './SearchOptionTabStyle';
 
 import { DateTapDisplay } from './OptionTabDisplay/DateTapDisplay';
 import { GuestTapDisplay } from './OptionTabDisplay/GuestTapDisplay';
@@ -12,31 +13,37 @@ import { FilterAddTapDisplay } from './OptionTabDisplay/FilterAddTapDisplay';
 
 import { SearchTabContext } from '../SearchTabs';
 
-
 const SearchOptionTabs = (props) => {
 
     const value = useContext(SearchTabContext);
 
-    const highlightStyle = {
-        background: "rgb(15,114,118)",
-        color: "white"
+    const SearchOptionTabStyle = styled.button`
+    display: inline-block;
+    width: auto;
+    height: auto;
+    padding-left: 15px;
+    padding-right: 15px;
+    padding-top: 8px;
+    padding-bottom: 8px;
+    background: white;
+    border: rgb(230,230,230) solid 1px; 
+    border-radius: 4px; 
+    font-size: 14px; 
+    font-weight: thin;
+    margin-left: 10px;
+    color: rgb(60,60,60); 
+    background: ${props => value.isTabActivated[props.name] ? "rgb(15,114,118)" : "white"};
+    color: ${props => value.isTabActivated[props.name] ? "white" : "black"};
+    &:hover {
+        ${props => value.isTabActivated[props.name] ? `background: rgb(30,90,93)`  : `background: rgb(230,230,230)`};
+        border-radius: none; 
+        outline: 0; ;
     }
-
-    const disableHighlightStyle = {
-        background: "white",
-        color: "black"
-    }
-
-    const highlightTab = (name) => {
-        if (value.isTabActivated[name]) {
-            return highlightStyle;
-        }
-        else return disableHighlightStyle;
-    }
+`    
 
 return (
     <Link to={`${value.match.url}/${props.type}`} onClick={(event) => value.passButtonClick(event, value.match.url)}>
-        <SearchOptionTabStyle style={highlightTab(props.type)} name={props.type}> {
+        <SearchOptionTabStyle name={props.type}> {
             (props.type === 'date' && <DateTapDisplay />) ||
             (props.type === 'guest' && <GuestTapDisplay />) ||
             (props.type === 'innType' && <InnTypeTapDisplay innTypes={value.innTypes} />) ||
@@ -48,9 +55,8 @@ return (
         </SearchOptionTabStyle>
     </Link>
 )
+
 }
-
-
 
 export { SearchOptionTabs }
 
